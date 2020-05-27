8f5ea9abf2cb05bfeaa2ec184c13bea4be035b75
tamburkey <petem14@gmail.com>
2020-05-27 19:46:45 -0400

    format / fix typo

--- errors2/main.js
+++ errors2/main.js
@@ -3,7 +3,7 @@ Errors2 (7 errors to fix)
 
 This page is not working.  It should take two numbers and display their sum on the page
 
-Not all of the errors will manifest ass “errors” in the inspector.  Two of the errors are “logical”.
+Not all of the errors will manifest as “errors” in the inspector.  Two of the errors are “logical”.
 
 */
 
@@ -12,13 +12,13 @@ const inputTwo = document.querySelector("#inputtwo");
 const addButton = document.querySelector("#addbutton");
 const resultDiv = document.querySelector("#resultdiv");
 
-function addNumbers(x, y){
+function addNumbers(x, y) {
     return x + y;
 }
 
-addButton.addEventListener("click", function(){
+addButton.addEventListener("click", function () {
     let numberOne = inputOne.value;
     let numberTwo = inputTwo.value;
     let sum = addnumbers(numberOne, numberTwo);
-    resultDiv.innerHTML +=  "<div>" + numberOne + " + " + numberTwo + " = " + sum + "</div>"
+    resultDiv.innerHTML += "<div>" + numberOne + " + " + numberTwo + " = " + sum + "</div>"
 })
