52bad7d4dd70f97a7755f9826318607c3c8255c7
tamburkey <petem14@gmail.com>
2020-05-27 19:41:08 -0400

    format and fix typos

--- errors1/main.js
+++ errors1/main.js
@@ -5,7 +5,7 @@ This page is not working.  There are two talking buttons (Dave and Malissa).
 
 When the Dave button is clicked, we should see “Hi my name is Dave. Thanks for clicking.” added to the page.
 
-When the Malissa is clicked, we should see "Malissa here! Why am I stuck in this button!?" added to the page.
+When the Malissa button is clicked, we should see "Malissa here! Why am I stuck in this button!?" added to the page.
 
 Your task is to find these 5 errors and fix them, so we have a working page.
 
@@ -15,20 +15,18 @@ const daveButton = document.querySelector("#dave-button");
 const malissaButton = document.querySelector("#malissa-button");
 const talkingSpaceDive = document.querySelector("#talking-space");
 
-
-
-function daveTalk(){
+function daveTalk() {
     return "Hi my name is Dave. Thanks for clicking.";
 }
 
-function malissaTalk(){
+function malissaTalk() {
     return "Malissa here! Why am I stuck in this button!?";
 }
 
-daveButton.addEventListener("click", function (){
-    talkingSpaceDive.innerHTML += "<div>" +daveTalk + "</div>"
+daveButton.addEventListener("click", function () {
+    talkingSpaceDive.innerHTML += "<div>" + daveTalk + "</div>"
 })
 
-malissaButton.addEventListener("click", function (){
-    talkingSpaceDive.innerHTML += "<div>" + malissaTalk + "</div>"    
+malissaButton.addEventListener("click", function () {
+    talkingSpaceDive.innerHTML += "<div>" + malissaTalk + "</div>"
 })
